8ee44d36a4889872f69861c7e77c51d625b777cb
Tobin-jn <tobin.j.n@gmail.com>
2019-01-08 14:59:22 -0700

    Make three routes on App

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/DailyPic">Picture of the Day</router-link>
+      <router-link to="/MonthlyPics">Pictures of the Month</router-link>
     </div>
     <router-view/>
   </div>
