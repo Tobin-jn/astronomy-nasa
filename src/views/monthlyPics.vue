5190a2329c8d794409e42e5b37dd2771d03e130f
Tobin-jn <tobin.j.n@gmail.com>
2019-01-08 15:19:06 -0700

    Add links to other pages

--- src/views/monthlyPics.vue
+++ src/views/monthlyPics.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="monthlyPics">
     <h1>This is an MONTHLY page</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/DailyPic">Picture of the Day</router-link>
   </div>
 </template>
