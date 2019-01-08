3d660809a0646afcf7cec2230bc08c0295a2d85b
Tobin-jn <tobin.j.n@gmail.com>
2019-01-08 15:17:07 -0700

    Cleaned out unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/DailyPic">Picture of the Day</router-link>
-      <router-link to="/MonthlyPics">Pictures of the Month</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
-
-<style>
-
-</style>
