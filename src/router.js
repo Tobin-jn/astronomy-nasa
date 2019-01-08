9f8cb464d23242beee7b23943756eccf244e2ee2
Tobin-jn <tobin.j.n@gmail.com>
2019-01-08 14:59:42 -0700

    Add to routes to router

--- src/router.js
+++ src/router.js
@@ -14,13 +14,22 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/DailyPic",
+      name: "dailyPic",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "./views/dailyPic.vue")
+    },
+    {
+      path: "/MonthlyPics",
+      name: "monthlyPics",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/monthlyPics.vue")
     }
   ]
 });
