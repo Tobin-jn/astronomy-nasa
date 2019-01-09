edd7aec92d9aa7931618ab8dcb5c194758261a9a
Tobin-jn <tobin.j.n@gmail.com>
2019-01-09 11:21:14 -0700

    Add tests to Home

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -2,11 +2,11 @@ import { shallowMount, mount } from "@vue/test-utils";
 import Home from "@/views/Home.vue";
 
 describe('Home', () => {
-  test('is a Vue instance', () => {
+  it('is a Vue instance', () => {
     const wrapper = mount(Home)
     expect(wrapper.isVueInstance()).toBeTruthy()
   });
-  test('renders correctly', () => {
+  it('renders correctly', () => {
     const wrapper = mount(Home)
     expect(wrapper.element).toMatchSnapshot()
   });
