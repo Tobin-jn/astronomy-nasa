b0ac3fdfa535a21d1ee6bfc8a92a976207ea8e4b
Tobin-jn <tobin.j.n@gmail.com>
2019-01-09 06:11:09 -0700

    Add ids

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,103 +1,126 @@
 const nasaData = [
-    {
-        copyright: "NASA",
-        date: "2019-01-01",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1812/WitchHeadNebula_Mishra_2725.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-02",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-03",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/orionred_WISEantonucci_1824.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-04",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/20190102UltimaThule-pr.png"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-05",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/UT-blink_3d_a.gif"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-06",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/Jan3yutu2CNSA.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-07",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/vltlaser_beletsky_1400.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-08",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1901/Geminids46P_jcc_2000.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-09",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1812/CarinaNebulaGerminiani.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-10",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1812/NewHorizonsApproachesMU69_1024.jpg"
-    },
-    {
-        copyright: "NASA",
-        date: "2019-01-11",
-        explanation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        media_type: "image",
-        service_version: "v1",
-        title: "Space Beauty and Wonder",
-        url: "https://apod.nasa.gov/apod/image/1812/NGC1365_HaLRGBpugh.jpg"
-    }   
-]
+  {
+    id: 1,
+    copyright: "NASA",
+    date: "2019-01-01",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1812/WitchHeadNebula_Mishra_2725.jpg"
+  },
+  {
+    id: 2,
+    copyright: "NASA",
+    date: "2019-01-02",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/sombrero_spitzer_3000.jpg"
+  },
+  {
+    id: 3,
+    copyright: "NASA",
+    date: "2019-01-03",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/orionred_WISEantonucci_1824.jpg"
+  },
+  {
+    id: 4,
+    copyright: "NASA",
+    date: "2019-01-04",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/20190102UltimaThule-pr.png"
+  },
+  {
+    id: 5,
+    copyright: "NASA",
+    date: "2019-01-05",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/UT-blink_3d_a.gif"
+  },
+  {
+    id: 6,
+    copyright: "NASA",
+    date: "2019-01-06",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/Jan3yutu2CNSA.jpg"
+  },
+  {
+    id: 7,
+    copyright: "NASA",
+    date: "2019-01-07",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/vltlaser_beletsky_1400.jpg"
+  },
+  {
+    id: 8,
+    copyright: "NASA",
+    date: "2019-01-08",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1901/Geminids46P_jcc_2000.jpg"
+  },
+  {
+    id: 9,
+    copyright: "NASA",
+    date: "2019-01-09",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1812/CarinaNebulaGerminiani.jpg"
+  },
+  {
+    id: 10,
+    copyright: "NASA",
+    date: "2019-01-10",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url:
+      "https://apod.nasa.gov/apod/image/1812/NewHorizonsApproachesMU69_1024.jpg"
+  },
+  {
+    id: 11,
+    copyright: "NASA",
+    date: "2019-01-11",
+    explanation:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    media_type: "image",
+    service_version: "v1",
+    title: "Space Beauty and Wonder",
+    url: "https://apod.nasa.gov/apod/image/1812/NGC1365_HaLRGBpugh.jpg"
+  }
+];
 
 export default nasaData;
