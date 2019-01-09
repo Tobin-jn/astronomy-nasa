4374a60e1e013c1b8ea228c95db8bbed6d085af4
Tobin-jn <tobin.j.n@gmail.com>
2019-01-08 21:11:06 -0700

    Write monthly pics component

--- src/views/monthlyPics.vue
+++ src/views/monthlyPics.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="monthlyPics">
-    <h1>This is an MONTHLY page</h1>
+    <h1>This is MONTHLY page</h1>
     <router-link to="/">Home</router-link>
     <router-link to="/DailyPic">Picture of the Day</router-link>
+
+    <div v-for="day in days" class='day-card'>
+      <h4> {{day.date}} </h4>
+      <img v-bind:src="day.url" class="day-image" alt="Picture of the Day" width="200" />
+    </div>
   </div>
 </template>
 
 <script>
+  import nasaData from '../assets/data'
+
+  export default {
+    data() {
+      return {
+        days: nasaData
+      }
+
+    },
+    methods: {
+
+    },
+    // created() {
+    //   this.$http.get('url....where...we..get..pic').then(function(data) {
+    //     const dayPics = [...this.days, data]
+    //     this.days = dayPics
+    //   })
+    // }
+  }
 
 </script>
 
